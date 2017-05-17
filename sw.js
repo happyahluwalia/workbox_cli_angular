7f5b2e654a6430d11296d33c88e8bfc3e0e9ab23
happyahluwalia <happyahluwalia@gmail.com>
2017-05-17 10:49:51 -0400

    Auto-generated commit

--- sw.js
+++ sw.js
@@ -19,35 +19,35 @@ importScripts('workbox-sw.prod.v0.0.2.js');
  */
 const fileManifest = [
   {
-    "url": "favicon.ico",
+    "url": "/favicon.ico",
     "revision": "b9aa7c338693424aae99599bec875b5f"
   },
   {
-    "url": "index.html",
+    "url": "/index.html",
     "revision": "6cf204a0a953ee40d3fa141b7956f229"
   },
   {
-    "url": "inline.57d2da1b2c25fa8b4b09.bundle.js",
+    "url": "/inline.57d2da1b2c25fa8b4b09.bundle.js",
     "revision": "ec805a12fa23cc612b6febda9dccc3de"
   },
   {
-    "url": "main.0fe8bbd848053a361a01.bundle.js",
+    "url": "/main.0fe8bbd848053a361a01.bundle.js",
     "revision": "e379a678a5d0ced08547a0e39d4dd0c7"
   },
   {
-    "url": "polyfills.60187f338ba5b0a175a8.bundle.js",
+    "url": "/polyfills.60187f338ba5b0a175a8.bundle.js",
     "revision": "e9c9878491f71e039235b9d3ace5bb0e"
   },
   {
-    "url": "styles.d41d8cd98f00b204e980.bundle.css",
+    "url": "/styles.d41d8cd98f00b204e980.bundle.css",
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   },
   {
-    "url": "vendor.020ca57fbacab669a996.bundle.js",
+    "url": "/vendor.020ca57fbacab669a996.bundle.js",
     "revision": "d4ebb9ddcecff5ba8fcc65b6f228b4d5"
   },
   {
-    "url": "workbox-sw.prod.v0.0.2.js",
+    "url": "/workbox-sw.prod.v0.0.2.js",
     "revision": "adecd1be2876458436c6fb61554335ce"
   }
 ];

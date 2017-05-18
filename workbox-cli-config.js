cd672996928fe6974536c3390127b2d33c13cbf9
happyahluwalia <happyahluwalia@gmail.com>
2017-05-18 13:08:16 -0400

    Enabled Google Analytics

---
+++ workbox-cli-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "globDirectory": "dist\\",
+  "globPatterns": [
+    "**/*.{js,png,ico,html,css}"
+  ],
+  "swDest": "build/sw.js",
+  "globIgnores": [
+    "..\\workbox-cli-config.js"
+  ]
+};
